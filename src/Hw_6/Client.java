40fd14960ec4c3a657d69a257d5c8ebd29c6bd8a
Alexey <traf10@bk.ru>
2021-08-10 10:19:11 +0700

    hw_6

--- src/Hw_6/Client.java
+++ src/Hw_6/Client.java
@@ -1,4 +1,58 @@
 package Hw_6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    private static final String SERV_ADDRESS = "localhost";
+    private static final int SERV_PORT = 8189;
+
+    public static void main(String[] args) {
+        Socket socket = null;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            socket = new Socket(SERV_ADDRESS, SERV_PORT);
+            System.out.println("Connected to server: " + socket.getRemoteSocketAddress());
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+
+            Thread reader = new Thread(() -> {
+                try {
+                    while (true) {
+                        outputStream.writeUTF(sc.nextLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            reader.setDaemon(true);
+            reader.start();
+
+            while (true) {
+                String str = inputStream.readUTF();
+                if (str.equals("/close")) {
+                    System.out.println("Lose connection to server");
+                    outputStream.writeUTF("/close");
+                    break;
+                } else {
+                    System.out.println("Server: " + str);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
--- src/Hw_6/Server.java
+++ src/Hw_6/Server.java
@@ -1,4 +1,56 @@
 package Hw_6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String[] args) {
+        Socket clientSoc = null;
+        Scanner sc = new Scanner(System.in);
+
+        try (ServerSocket serverSoc = new ServerSocket(8189)) {
+            System.out.println("Server on");
+            clientSoc = serverSoc.accept();
+            System.out.println("Client connect: " + clientSoc.getRemoteSocketAddress());
+            DataInputStream inputStream = new DataInputStream(clientSoc.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(clientSoc.getOutputStream());
+
+
+            Thread reader = new Thread(() -> {
+                try {
+                    while (true) {
+                        outputStream.writeUTF(sc.nextLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            reader.setDaemon(true);
+            reader.start();
+
+            while (true) {
+                String str = inputStream.readUTF();
+                if (str.equals("/close")) {
+                    System.out.println("Client leave server");
+                    outputStream.writeUTF("/close");
+                    break;
+                } else {
+                    System.out.println("Client: " + str);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSoc.close();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
